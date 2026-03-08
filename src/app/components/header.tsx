199938e4506b6963837935fe5b495a3a940945de
Ohiro Jude <judoski87@gmail.com>
2026-03-08 14:08:56 +0100

    Add framer motion underline for active nav link

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,11 @@
+'use client';
+
+import clsx from 'clsx';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import Logo from './logo';
+
+import { motion } from 'framer-motion';
 
 const routes = [
     {
@@ -10,18 +16,32 @@ const routes = [
 ];
 
 export default function Header() {
+    const activePathName = usePathname();
+
     return (
         <header className='flex items-center justify-between px-3 sm:px-9 border-b border-white/10 h-14'>
             <Logo />
 
-            <nav>
-                <ul className='flex gap-x-6 text-white/50 text-sm '>
+            <nav className='h-full'>
+                <ul className='flex gap-x-6 text-white/50 text-sm h-full'>
                     {routes.map(route => (
                         <li
                             key={route.path}
-                            className='hover:text-white transition'
+                            className={clsx(
+                                'hover:text-white flex items-center transition relative',
+                                {
+                                    'text-white': activePathName === route.path,
+                                }
+                            )}
                         >
                             <Link href={route.path}>{route.name}</Link>
+
+                            {activePathName === route.path && (
+                                <motion.div
+                                    layoutId='header-active-link'
+                                    className='w-full bg-accent h-1 absolute bottom-0'
+                                ></motion.div>
+                            )}
                         </li>
                     ))}
                 </ul>
