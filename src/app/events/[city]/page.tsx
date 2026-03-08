b0bfe660269daa941b78ef56b17ef03fa3a1e9c2
Ohiro Jude <judoski87@gmail.com>
2026-03-08 19:37:11 +0100

    Add scrollbar to events page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,8 +2,8 @@ import H1 from '@/app/components/h1';
 
 export default function EventsPage() {
     return (
-        <main>
-            <H1>EventsPage</H1>
+        <main className='flex flex-col items-center px-[20px] py-24 min-h-[110vh]'>
+            <H1>Events in Austin</H1>
         </main>
     );
 }
